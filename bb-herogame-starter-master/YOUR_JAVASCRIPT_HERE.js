f7c9d2ba470dd760d5fe3bf5f03b30bf828fb86d
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-01 08:58:37 +0200

    add hero property

--- bb-herogame-starter-master/YOUR_JAVASCRIPT_HERE.js
+++ bb-herogame-starter-master/YOUR_JAVASCRIPT_HERE.js
@@ -1,5 +1,12 @@
 // Write your JS here
 
 let hero = {
-    
+    name: 'Hero',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'wapon-1',
+        damage: 2
+    }
 }
